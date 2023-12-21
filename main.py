ddae795c7778a08155f7e67b9f0c4eaa59d3967b
banjo_dracer <63825976+dracer001@users.noreply.github.com>
2023-12-21 07:57:48 +0100

    Update main.py
    Reloadinf Lyrics

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import shutil
 import speech_recognition as sr
 from gtts import gTTS
 
-from transformers import pipeline
-
 app = FastAPI()
 
 def transcribe_audio(audio_file_path):
@@ -16,18 +14,6 @@ def transcribe_audio(audio_file_path):
         audio_data = recognizer.record(source)
         text = recognizer.recognize_google(audio_data)
         return text
-
-
-def genLyrics(input_text):
-    # Load the text generation pipeline using a specific model
-    text_generator = pipeline("text-generation", model="distilgpt2")
-
-    # Example input text as a prompt
-   
-
-    # Generate text based on the input prompt
-    generated_text = text_generator(input_text, max_length=50, num_return_sequences=1)
-    return generated_text[0]['generated_text']
 
 
 @app.get("/")
@@ -48,10 +34,6 @@ async def upload_audio_file(file: UploadFile = File(...)):
         # Transcribe the audio to text
         transcription = transcribe_audio(file_location)
 
-        new_lyrics = genLyrics(transcription)
-        # new_lyrics = generateLyrics(transcription, 50)
-        # Optionally, remove the temporary uploaded file
-       
         # speech = gTTS(text=new_lyrics, lang='en', slow=False)
 # # Saving the converted audio in a file (you can also use .mp3 instead of .wav)
         # speech.save("output.wav")
@@ -60,7 +42,7 @@ async def upload_audio_file(file: UploadFile = File(...)):
             # audio_content = audio_file.read()
         # os.remove(file_location)
         # return StreamingResponse(io.BytesIO(audio_content), media_type="audio/wav")
-        return {"filename": file.filename, "transcription": new_lyrics}
+        return {"filename": file.filename, "transcription": transcription}
     except Exception as e:
         return {"error": str(e)}
 
